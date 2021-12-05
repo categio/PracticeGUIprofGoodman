28694ec12881eb19868843f9977b722cd8610451
Categio <categio1@gmail.com>
2021-12-04 22:34:24 -0800

    Added class Show to project

--- src/data/Database.java
+++ src/data/Database.java
@@ -39,4 +39,56 @@ public class Database {
             }
         }
     }
+    public static ArrayList<Genre> getAllGenres() {
+        connect();
+        ArrayList<Genre>  genres = new ArrayList<Genre>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(GET_ALL_GENRES_SQL);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                genres.add(newGenre(rs.getString( "genre")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return genres;
+    }
+    public static ArrayList<ShowType> getAllShowTypes() {
+        connect();
+        ArrayList<ShowType> types = new ArrayList<ShowType>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(GET_ALL_TYPES_SQL);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                types.add(new ShowType(rs.getString("titleType")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return types;
+    }
+    public static ArrayList<Shows> findShows(Integer minShows, String titleType, String genre) {
+        connect();
+        ArrayList<Show> shows = new ArrayList<Show>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(FIND_SHOWS_SQL);
+            stmt.setInt(0, minShows);
+            stmt.setString(1, titleType);
+            stmt.setString(2,genre);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                shows.add(new Show(
+                        //primaryTitle, startYear, averageRating, numVotes
+                        rs.getString("primaryTitle"),
+                        rs.getInt("startYear"),
+                        rs.getFloat("averageRating"),
+                        rs.getInt("numVotes")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return shows;
+    }
+
 }
---
+++ src/logic/Show.java
@@ -0,0 +1,8 @@
+package logic;
+
+/**
+ * @author Catherine Giovetti
+ * @version 2021.12.04
+ */
+public class Show {
+}
